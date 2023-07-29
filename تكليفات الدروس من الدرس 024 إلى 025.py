7b8441f40bd4f1a824c2e10fc937741f4043ebae
ahmedtaha89 <ahmed_947a@fci.kfs.edu.eg>
2023-07-30 01:11:09 +0300

    تكليفات الدروس

---
+++ تكليفات الدروس من الدرس 024 إلى 025.py
@@ -0,0 +1,31 @@
+# تكليفات الدروس من الدرس 024 إلى 025
+
+# التكليف 01
+Name = 'Ahmed',
+print(Name)
+print(type(Name))
+
+
+# التكليف 02
+friends = ("Osama", "Ahmed", "Sayed")
+New_List = list(friends)
+New_List[0] = 'Elzero'
+New_Tuple = tuple(New_List)
+print(New_Tuple)
+
+
+# التكليف 03
+nums = (1, 2, 3)
+letters = ("A", "B", "C")
+concat = nums+letters
+print(concat)
+print(len(concat))
+
+
+# التكليف 04
+my_tuple = (1, 2, 3, 4)
+a , b , _ , c = my_tuple
+print(a)
+print(b)
+print(c)
+
